c49f2eaa3ae79eae3e7872f7d9887d4afe8ec04a
auys93 <auys93@gmail.com>
2017-11-04 16:10:17 +0800

    Fixed login bug

--- frontend/checkLogin.js
+++ frontend/checkLogin.js
@@ -9,31 +9,33 @@ app.controller('loginCtrl', function($scope, $http) {
   $scope.lblMsg = null;
   $scope.test = [];
 
+  //console.log(data);
+  $http.get("http://localhost:4567/staff")
+    .then(function(response) {
+      $scope.data = response.data;
+    });
+
+
   $scope.checkStaffData = function(username, password) {
     var data = {
       username: username,
       password: password
     };
-    //console.log(data);
-    $http.get("http://localhost:4567/staff")
-    .then(function(response) {
-      $scope.data = response.data;
-    });
 
     var loggedin = "";
-    angular.forEach($scope.data, function(item){
+    angular.forEach($scope.data, function(item) {
 
-    if ((item.username == data.username) && (item.password = data.password)){
-              console.log("Success");
-              loggedin = "1";
-                window.location.href = 'index.html';
-              }
-              message = 'Username or password is incorrect';
-        });
+      if ((item.username == data.username) && (item.password = data.password)) {
+        console.log("Success");
+        loggedin = "1";
+        window.location.href = 'index.html';
+      }
+      message = 'Username or password is incorrect';
+    });
 
-        if(loggedin != "1")
-        alert("Your username or password is incorrect.\nPlease try again.");
-    };
+    if (loggedin != "1")
+      alert("Your username or password is incorrect.\nPlease try again.");
+  };
 
 
 });
@@ -42,12 +44,12 @@ app.controller('loginCtrl', function($scope, $http) {
 //});
 
 function clearForm() {
-   // Get the first form with the name
-   // Usually the form name is not repeated
-   // but duplicate names are possible in HTML
-   // Therefore to work around the issue, enforce the correct index
-   var frm = document.getElementsByName('staff-form')[0];
-   frm.submit(); // Submit the form
-   frm.reset();  // Reset all form data
-   return false; // Prevent page refresh
+  // Get the first form with the name
+  // Usually the form name is not repeated
+  // but duplicate names are possible in HTML
+  // Therefore to work around the issue, enforce the correct index
+  var frm = document.getElementsByName('staff-form')[0];
+  frm.submit(); // Submit the form
+  frm.reset(); // Reset all form data
+  return false; // Prevent page refresh
 }
